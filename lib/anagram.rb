827bc40df6697d8c109e043c5c26759e524173de
michaelstsnow <michael.st.snow@gmail.com>
2018-11-22 18:26:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Anagram
   #create the anagram
+  attr_accessor(:word)
   def initialize(word)
     @word=word
   end
   def match(array_possible_anagrams)
     matching_words=[]
     array_possible_anagrams.each do |possible_match|
-
+      
     end
   end
 end
